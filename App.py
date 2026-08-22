14443892f28b5bdfe5ffcdff14d741893b2db871
Abhi <sutharabhimanyu70@gmail.com>
2026-08-22 11:28:25 +0530

    Update code changes

--- App.py
+++ App.py
@@ -40,7 +40,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # --- Active Tunnel Link ---
-OLLAMA_SERVER_URL = "https://washington-secret-resolutions-muze.trycloudflare.com"
+OLLAMA_SERVER_URL = "https://received-candidate-premises-andrea.trycloudflare.com"
 
 # --- Database Setup ---
 conn = sqlite3.connect("ai_assistant.db", check_same_thread=False)
