0e53709bc17f25cfbde188f6819e0d5391803b80
Abhi <sutharabhimanyu70@gmail.com>
2026-08-23 21:43:54 +0530

    Update code changes

--- App.py
+++ App.py
@@ -192,7 +192,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # --- Active Tunnel Link ---
-OLLAMA_SERVER_URL = "https://exit-joining-camcorder-montreal.trycloudflare.com"
+OLLAMA_SERVER_URL = "https://member-marco-pieces-improvements.trycloudflare.com"
 
 # --- Database Setup ---
 conn = sqlite3.connect("ai_assistant.db", check_same_thread=False)
