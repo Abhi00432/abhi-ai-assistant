8b6f41a8967ffa9f3c354b5c10d8b2fba852325d
Abhi <sutharabhimanyu70@gmail.com>
2026-08-23 17:11:07 +0530

    Update code changes

--- App.py
+++ App.py
@@ -192,7 +192,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # --- Active Tunnel Link ---
-OLLAMA_SERVER_URL = "https://logged-regardless-soma-casting.trycloudflare.com"
+OLLAMA_SERVER_URL = "https://votes-planned-reminder-chairs.trycloudflare.com"
 
 # --- Database Setup ---
 conn = sqlite3.connect("ai_assistant.db", check_same_thread=False)
