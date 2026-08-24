9db8e8c0d86fcd7d49c0628012ffdfdeb5c24b74
Abhi <sutharabhimanyu70@gmail.com>
2026-08-24 21:19:04 +0530

    Update code changes

--- App.py
+++ App.py
@@ -192,7 +192,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # --- Active Tunnel Link ---
-OLLAMA_SERVER_URL = "https://game-camcorder-tackle-turtle.trycloudflare.com"
+OLLAMA_SERVER_URL = "https://digital-heather-prairie-leo.trycloudflare.com"
 
 # --- Database Setup ---
 conn = sqlite3.connect("ai_assistant.db", check_same_thread=False)
