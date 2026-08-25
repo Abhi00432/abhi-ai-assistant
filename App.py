03d92e395f09061b1a294707b3b361546b3ee7c3
Abhi <sutharabhimanyu70@gmail.com>
2026-08-25 10:06:37 +0530

    Update code changes

--- App.py
+++ App.py
@@ -192,7 +192,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # --- Active Tunnel Link ---
-OLLAMA_SERVER_URL = "https://menus-sleeps-possibilities-armor.trycloudflare.com"
+OLLAMA_SERVER_URL = "https://glory-dui-workshop-prostores.trycloudflare.com"
 
 # --- Database Setup ---
 conn = sqlite3.connect("ai_assistant.db", check_same_thread=False)
