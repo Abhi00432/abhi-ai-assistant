031a2cd964ecf328ddbf33e11e91acfe4ee8b174
Abhi <sutharabhimanyu70@gmail.com>
2026-08-25 09:57:31 +0530

    Update code changes

--- App.py
+++ App.py
@@ -192,7 +192,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # --- Active Tunnel Link ---
-OLLAMA_SERVER_URL = " https://wake-figure-antiques-tub.trycloudflare.com"
+OLLAMA_SERVER_URL = "https://menus-sleeps-possibilities-armor.trycloudflare.com"
 
 # --- Database Setup ---
 conn = sqlite3.connect("ai_assistant.db", check_same_thread=False)
