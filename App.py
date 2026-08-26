846bb69d6ece9ee5c36a2407c2a8b22176df2e4b
Abhi <sutharabhimanyu70@gmail.com>
2026-08-26 16:17:13 +0530

    Update code changes

--- App.py
+++ App.py
@@ -192,7 +192,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # --- Active Tunnel Link ---
-OLLAMA_SERVER_URL = "https://lil-association-fraser-pork.trycloudflare.com"
+OLLAMA_SERVER_URL = " https://whereas-offset-anniversary-characterized.trycloudflare.com"
 
 # --- Database Setup ---
 conn = sqlite3.connect("ai_assistant.db", check_same_thread=False)
