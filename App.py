fe85db181c54d249daf47f2f2131c3a453a26e6f
Abhi <sutharabhimanyu70@gmail.com>
2026-08-31 21:20:31 +0530

    Update code changes

--- App.py
+++ App.py
@@ -321,7 +321,7 @@ def load_session_messages(session_id: int):
 # ----------------------------------------------------
 # 4. Backend Tunnel Endpoint
 # ----------------------------------------------------
-OLLAMA_BASE_URL = "https://wake-figure-antiques-tub.trycloudflare.com".strip()
+OLLAMA_BASE_URL = "https://adsl-juice-label-greetings.trycloudflare.com".strip()
 
 # ----------------------------------------------------
 # 5. Persistent Authentication Controller (Auto-Login)
