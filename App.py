794c1e0243070f039cc3ce73cac9d8d75fe04a80
Abhimanyu <sutharabhimanyu70@gmail.com>
2026-09-09 20:25:59 +0530

    Update App.py

--- App.py
+++ App.py
@@ -321,7 +321,7 @@ def load_session_messages(session_id: int):
 # ----------------------------------------------------
 # 4. Backend Tunnel Endpoint
 # ----------------------------------------------------
-OLLAMA_BASE_URL = "https://commit-intersection-midwest-classified.trycloudflare.com".strip()
+OLLAMA_BASE_URL = "https://notify-nations-introduced-meaning.trycloudflare.com".strip()
 
 # ----------------------------------------------------
 # 5. Persistent Authentication Controller (Auto-Login)
